1a1e09bbfd802f0577b854523a5b8a91d46b1fd6
Ray <ray@4dvw.com>
2017-12-27 10:25:56 -0500

    make footer and nav into js string

---
+++ components/user-nav.js
@@ -0,0 +1,27 @@
+
+var userNav = '<nav class="main-nav blue transparent stick-fixed">'+
+'    <div class="full-wrapper relative clearfix"> '+
+'        <div class="nav-logo-wrap local-scroll">'+
+'            <a href="mp-index.html" class="logo">'+
+'                <img src="images/logo-white.png" alt="" />'+
+'                <span>Virtual World</span>'+
+'            </a>'+
+'        </div>'+
+'        <div class="mobile-nav">'+
+'            <i class="fa fa-bars"></i>'+
+'        </div> '+
+'        <div class="inner-nav desktop-nav">'+
+'            <ul class="clearlist"> '+
+'                <li>'+
+'                    <a> </a>'+
+'                </li> '+
+'                <li>'+
+'                    <a href="#" class="" style="font-size:12px;">'+
+'                        <i class="fa fa-user yourHomeIcon" style="font-size:18px;"></i> Your Home</a>'+
+'                </li> '+
+'            </ul>'+
+'        </div> '+
+'    </div>'+
+'</nav>';
+	
+document.querySelector(".userNav").innerHTML = userNav;
