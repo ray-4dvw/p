7c5f93dede208310df0db208d83f80978f9c2737
SergeiVilbik <sergei@4dvw.com>
2018-02-06 09:31:39 -0500

    fixed footer links

--- components/user-footer.js
+++ components/user-footer.js
@@ -6,8 +6,10 @@ var userFooter = '<footer class="page-section bg-darker footer pb-40 pt-80">'+
 '                <a class="logo"  href="http://www.4dvw.com">'+
 '                    <img src="images/logo-white.png" alt="" />'+
 '                </a>'+
+'				 <a href="http:/www.4dvw.com">'+
 '                <span>VIRTUAL'+
 '                    <br/>WORLD</span>'+
+'				 </a>' +
 '            </div>'+
 '            <small style="margin-left:5px;">©2017 4D Virtual World ALL RIGHTS RESERVED</small>'+
 '        </div>'+
@@ -20,10 +22,7 @@ var userFooter = '<footer class="page-section bg-darker footer pb-40 pt-80">'+
 '                <a href="http://www.4dvw.com/investor_relations">INVESTOR RELATIONS</a>'+
 '            </div>'+
 '            <div>'+
-'                <a href="http://www.4dvw.com/">YOUR HOME</a>'+
-'            </div>'+
-'            <div>'+
-'                <a href="javascript:void(0)">Terms and Conditions</a>'+
+'                <a href="http://www.4dvw.com/terms_and_conditions">Terms and Conditions</a>'+
 '            </div>'+
 '            <div class="footerSocial mt-10">'+
 '                    <a href="javascript:void(0)" title="Facebook" target="_blank"><i class="fa fa-facebook"></i></a>'+
@@ -38,7 +37,7 @@ var userFooter = '<footer class="page-section bg-darker footer pb-40 pt-80">'+
 '                <div><a>416.275.2662'+
 '                </a></div>'+
 '                <div><a>Greg@4DVW.com </a></div>'+
-'                <div><a>67 Yonge Street, Suite 903'+
+'                <div><a href="https://www.google.ca/maps/place/207+Queens+Quay+W,+Toronto,+ON+M5J+1A7/@43.6387842,-79.3829653,17z/data=!3m1!4b1!4m5!3m4!1s0x882b34d557417d69:0x238e784027f661d9!8m2!3d43.6387803!4d-79.3807766">67 Yonge Street, Suite 903'+
 '                        <br/> Toronto, ON M5E 1J8, Canada'+
 '                    </a></div>'+
 '            </div>                   '+
